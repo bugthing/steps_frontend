c7e19526750c1582a79d80292bf5b9dd470914bd
Benjamin Martin <benjamin247365@hotmail.com>
2013-09-15 19:37:50 +0100

    Added new chart functionality

--- js/controllers.js
+++ js/controllers.js
@@ -1,2 +1,25 @@
 
 
+Steps.ChartsNewController =  Ember.ArrayController.extend({
+  actions: {
+    createChart: function () {
+
+      // Get the todo title set by the "New Todo" text field
+      var title = this.get('newTitle');
+      if (!title.trim()) { return; }
+
+      // Create the new Todo model
+      var todo = this.store.createRecord('chart', {
+        title: title
+      });
+
+      // Clear the "New Chary" text field
+      this.set('newTitle', '');
+
+      // Save the new model
+      todo.save();
+    }
+  }
+});
+
+
